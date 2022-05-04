1172b41444004920a4edd4233da14f063b2fa08a
kishanc2 <kishan.challagulla@icloud.com>
2022-05-04 10:18:15 -0500

    correct link to gthub

--- components/navbar.js
+++ components/navbar.js
@@ -86,7 +86,7 @@ const Navbar = props => {
             style={{ gap: 4 }}
             pl={2}
           >
-            <IoLogoGithub />
+            <IoLogoGithub /> 
             GitHub
           </LinkItem>
         </Stack>
