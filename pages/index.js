3f9fa3b98a108eea66a25f5dd895151b8c0ba876
kishanc2 <kishan.challagulla@icloud.com>
2022-05-04 23:05:06 -0500

    imge removal

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ import thumbYouTube from '../public/images/links/youtube.png'
 import thumbInkdrop from '../public/images/works/inkdrop_eyecatch.png'
 import Image from 'next/image'
 
-const ProfileImage = chakra(Image, {
+const ProfileImage = chakra({
   shouldForwardProp: prop => ['width', 'height', 'src', 'alt'].includes(prop)
 })
 
