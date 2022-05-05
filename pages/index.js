069cb47a7459ddd3c6cdd8fd9003029603d219b8
kishanc2 <kishan.challagulla@icloud.com>
2022-05-04 23:54:43 -0500

    g

--- pages/index.js
+++ pages/index.js
@@ -20,9 +20,9 @@ import { GridItem } from '../components/grid-item'
 import { IoLogoTwitter, IoLogoInstagram, IoLogoGithub } from 'react-icons/io5'
 import thumbYouTube from '../public/images/links/youtube.png'
 import thumbInkdrop from '../public/images/works/inkdrop_eyecatch.png'
+import Image from 'next/image'
 
-
-const ProfileImage = chakra({
+const ProfileImage = chakra(Image, {
   shouldForwardProp: prop => ['width', 'height', 'src', 'alt'].includes(prop)
 })
 
@@ -46,6 +46,33 @@ const Home = () => (
             Kishan Challagulla
           </Heading>
           <p>Computer Science Student - the University of Illinois Urbana-Champaign</p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          textAlign="center"
+        >
+          <Box
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            w="100px"
+            h="100px"
+            display="inline-block"
+            borderRadius="full"
+            overflow="hidden"
+          >
+            <ProfileImage
+              // src="/images/takuya.jpg"
+              // borderColor = "whiteAlpha.800"
+              // fill = "Teal 200"
+              alt="Profile image"
+              borderRadius="full"
+              width="100%"
+              height="100%"
+            />
+          </Box>
         </Box>
       </Box>
 
